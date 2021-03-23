aee90d9803f7c07e971a0700f6e6b3aca64a902b
leandrobko <leandrobko@gmail.com>
2021-03-22 23:40:22 -0300

    learning more about Angular11

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router'
+import { HttpClientModule } from '@angular/common/http'
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Error404Component } from './error-404/error-404.component';
+import { CourseModule } from './courses/course.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    Error404Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CourseModule,
+    RouterModule.forRoot([
+      {
+        path: '', redirectTo: 'courses', pathMatch: 'full'
+      },
+      {
+        path: '**', component: Error404Component
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
